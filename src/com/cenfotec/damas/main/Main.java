4143153a8eb8e1f1712f74e9d901fa028141bdeb
Carolina Loaiza <karolina801chan@gmail.com>
2019-02-27 00:33:25 -0600

    wip

--- src/com/cenfotec/damas/main/Main.java
+++ src/com/cenfotec/damas/main/Main.java
@@ -1,15 +1,11 @@
 package com.cenfotec.damas.main;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Cursor;
-import java.awt.Dimension;
-import java.awt.EventQueue;
-import java.awt.Graphics;
 import java.awt.GridLayout;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -19,15 +15,18 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.LineBorder;
 
 public class Main {
 	private static List<String[]> Row = new ArrayList<String[]>();
 	private static String[] ColNames  = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"};
-	static ImageIcon iconWhite = new ImageIcon("Users/macbook/Documents/Damas_v1/blanca-02.png");
-	static ImageIcon iconBlack = new ImageIcon("Users/macbook/Documents/Damas_v1/negra-03.png");
+	private static String fichaBlanco = "https://res.cloudinary.com/testingapps/image/upload/v1551240020/blanca-02_imhu5t.png";
+	private static String fichaNegra = "https://res.cloudinary.com/testingapps/image/upload/v1551240020/negra-03_k4wtj9.png";
+	private static JPanel panel = new JPanel();
+	private static String ficha = "";
+	private static String movimiento = "";
+	private static String jugador = "";
+	private static String fichaJugador = "";
 	
     public static void main(String[] a) throws MalformedURLException {
         JFrame window = new JFrame();
@@ -35,58 +34,93 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         
-        JPanel panel = new JPanel();
-        initGame(panel);
+        initGame();
         panel.setLayout(new GridLayout(10,10));
         window.add(panel);
-        window.setSize(900, 740);
+        window.setSize(820, 800);
         window.setVisible(true);
-    } 
+    }
     
-    public static void initGame(JPanel panel) throws MalformedURLException {
+    public static void initGame() throws MalformedURLException {
     	int countCell = 0;
     	for (int e = 0; e <= 9; e++) {
     		String[] Col  = new String[10];
 	    	for (int i = 0; i <= 9; i++) {
-	    		//System.out.print("");
+	    		JButton CellButton = new JButton();
 	    		Col[i] = e + ColNames[i];
 	    		String id = Col[i];
 	    		if ((countCell % 2 != 0) && (e < 4)) {
-	    			id = id + "-Blanca";
+	    			id = id + "-Jugador01";
+	    			CellButton.setIcon(new ImageIcon(new URL(fichaBlanco)));
 	    		} else if ((countCell % 2 != 0) && (e > 5)) {
-	    			id = id + "-Negra";
+	    			id = id + "-Jugador02";
+	    			CellButton.setIcon(new ImageIcon(new URL(fichaNegra)));
 	    		}
 	    		countCell++;
-	    		JButton CellButton = new JButton(new ImageIcon(new URL("https://www.rgagnon.com/images/gumby.gif")));
+	    		
 	    		CellButton.setOpaque(true);
 	    		CellButton.setBorder(new LineBorder(Color.BLACK));
 	    		CellButton.setBackground(Color.WHITE);
 	    		if (countCell % 2 == 0) {
+	    			id = id + "-Negro";
 	    			CellButton.setBackground(Color.LIGHT_GRAY);
+	    		} else {
+	    			id = id + "-Blanco"; 
 	    		}
-	    		CellButton.setPreferredSize(new Dimension(70, 70));
 	    		CellButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 	    		CellButton.setActionCommand(id);
 	    		CellButton.addActionListener(new ActionListener() {
 	    			@Override
 					public void actionPerformed(ActionEvent e) {
-	    				String action = e.getActionCommand();
-	    				clickEvent(action, "");
+	    				try {
+							clickEvent(e);
+						} catch (MalformedURLException e1) {
+							e1.printStackTrace();
+						}
 					}
 	    		});
 	    		panel.add(CellButton);
-	    		//panel.add("ok");
 	    	}
 	    	countCell++;
 	    	Row.add(Col);
     	}
     }
     
-    public static void clickEvent(String position1, String position2) {
-    	System.out.print("----------");
-    	System.out.print(position1);
+    public static void clickEvent(ActionEvent elem) throws MalformedURLException {
+    	String action = elem.getActionCommand();
+    	if (action.contains("Jugador01") || action.contains("Jugador02")) {
+    		ficha = action;
+    		fichaJugador = action.contains("Jugador01") ? fichaBlanco : fichaNegra;
+    		jugador = ficha.contains("Jugador01") ? "-Jugador01" : "-Jugador02";
+    	} else if (action.contains("Negro") && ficha != "") {
+    		movimiento = action;
+    	}
+    	
+    	if ((ficha != "" && movimiento != "") && (ficha != movimiento)) {
+    		checkMovimiento();
+    		fichaJugador = "";
+    		ficha = "";
+    		movimiento = "";
+    	}
 	}
     
+    public static void checkMovimiento() throws MalformedURLException {
+    	Component[] comp = panel.getComponents();
+        for (int i = 0;i<comp.length;i++) {
+            if (comp[i] instanceof JButton) {
+            	String action = ((JButton)comp[i]).getActionCommand();
+            	if (movimiento != "" && action == movimiento && fichaJugador != "") {
+            		((JButton)comp[i]).setIcon(new ImageIcon(new URL(fichaJugador)));
+            		((JButton)comp[i]).setActionCommand(action+jugador);
+            	}
+            	if (ficha != "" && action == ficha) {
+            		((JButton)comp[i]).setIcon(null);
+            		String newAction = action.replace(jugador, "");
+            		((JButton)comp[i]).setActionCommand(newAction);
+            	}
+            }
+        }
+	}
     
 }
 
